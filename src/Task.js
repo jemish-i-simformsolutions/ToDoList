f395c78065d5f2d9922d0505e40b38cb42b4bb03
Jemish Italiya <italiyajemish99@gmail.com>
2021-01-15 11:50:47 +0530

    first commit

--- src/Task.js
+++ src/Task.js
@@ -1,29 +1,41 @@
 import React, { useState } from "react";
 import DeleteIcon from '@material-ui/icons/Delete';
-import "./Pro.css"
+import "./Pro.css";
 
 function Task(props) {
-    const [task1,setTask1]=useState("");
     const [arr,setArr]=useState([]);
+    const [listComponent,setListComponent]=useState();
+    
+    function display_list(val){
+        return(
+            <>
+            <div id='list'>
+            {val}
+            </div>
+            <br/>
+            </>
+        );
+
+    }
+    
+    function getTask(){
+        
+        setArr(x=>[(document.getElementById("textarea").value),...x]);  
+       
+        
+    }
+   
+   console.log(arr.length)
     return(
         <>
         <div id='task'>
         <textarea id="textarea"></textarea>
-         <button id="button1" onClick={()=>{
-            
-            setTask1(document.getElementById("textarea").value);
-            
-            document.getElementById('textarea').value="";
-            setArr(x=>[...x,task1]);
-            console.log(arr.toString())
-            
-             
-        }}>
-             Add</button>
+         <button id="button1" onClick={getTask}>Add</button>
+        
         </div>
         <div id="task_list">
-        {arr}
-    
+        <button onClick={()=>setListComponent(arr.map(display_list))}>Show your work list here</button><br/>
+         {listComponent}
         </div>
         
         </>
